b221ec9200c3fbcb78346315566f5c3931f958eb
Will Chou <will.chou@qlik.com>
2022-12-06 00:46:38 -0500

    day6 minor cleanups

--- src/day6.ts
+++ src/day6.ts
@@ -6,10 +6,16 @@ const data = await readFile(path)
 const input = data.toString()
 const sample = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
 
+interface Window {
+  [key: string]: number
+}
+
+function detectUniqueCharsInWindow(window: Window, size: number) {
+  return Object.values(window).length >= size && Object.values(window).every(val => val === 1)
+}
+
 function part1(input: string, messageSize = 4) {
-  const window: {
-    [key: string]: number
-  } = {}
+  const window: Window = {}
   let r = 0
   let l = 0
   while (r < input.length) {
@@ -19,7 +25,7 @@ function part1(input: string, messageSize = 4) {
 
     // Read value at right pointer
     window[input[r]] += 1
-    if (Object.values(window).length >= 4 && Object.values(window).filter(Boolean).every(val => val === 1)) {
+    if (detectUniqueCharsInWindow(window, messageSize)) {
       // Found window with unique chars
       break
     }
@@ -30,8 +36,13 @@ function part1(input: string, messageSize = 4) {
     } else if (r-l === messageSize -1) {
       // Slide window
       r += 1
+
       // Remove value at left prior to incrementing it
       window[input[l]] -= 1
+      if (window[input[l]] === 0) {
+        // Keep only keys with values
+        delete window[input[l]]
+      }
       l += 1
     }
   }
